3696d44860243979a099120323336e715ba321ad
Arnaud Clarat <arnaud.clarat@live.fr>
2024-10-12 11:37:54 +0200

    Ajoutez des fichiers projet.

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿class Person(string firstName, string lastName, DateTime birthDate)
+{
+    public string FirstName { get; set; } = firstName;
+    public string LastName { get; set; } = lastName;
+    public DateTime BirthDate { get; set; } = birthDate;
+}
